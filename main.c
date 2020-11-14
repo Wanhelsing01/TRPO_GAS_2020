cf2c546bec58e28f6a08caf9eadf6f6272d15b10
NatalyLiss <natalyliss@mail.ru>
2020-11-14 17:01:34 +0300

    FFT writen to dat file

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 
 #include "def_types.h"
 #include "readWavFuncs.h"
-
+#include "fftw3.h"
 
 /*
 Путь к wav-файлу можно задать из командной строки,
@@ -41,13 +41,45 @@ int main(int argc, char** argv) {
   readHeader(&header, f_in);
 
   int N = 512;
+
+
   // Calculate the number of portions and the tail size
   int portions_info[2]; //[PortionQuantity, TailSize]
   calcPortions(&portions_info[0], &header, N);
 
   // Reading portions till the end in cycle
-  float data_portion[N];
-  for(int i = 0; i < portions_info[0]; ++i){
+  double data_portion[N];
+
+  readData(&data_portion[0], N, f_in);
+  
+  char* f_out_name = "output.dat";
+  FILE* f_out = fopen(f_out_name, "wb");
+  if(f_out == NULL){
+    printf("ERROR in OPEN FILE [%s] \n", f_out_name);
+	  return 1;
+  } else {
+    printf("OPEN FILE [%s] is OK \n", f_out_name);
+  }
+  fftw_complex out[N/2+1];
+  //test fft
+  fftw_plan plan;
+  plan = fftw_plan_dft_r2c_1d(N, data_portion, out,  FFTW_ESTIMATE);
+
+  fftw_execute(plan);
+
+  fftw_destroy_plan(plan);
+  int j;
+  double sq[N/2+1];
+  for(j=0;j<N/2+1;j++){
+
+    sq[j]=sqrt((out[j][0])*(out[j][0])+(out[j][1])*(out[j][1]));
+    fprintf(f_out, "%f\n",sq[j]);
+  }
+
+
+
+  int i;
+  for(i = 0; i < portions_info[0]; ++i){
       readData(&data_portion[0], N, f_in );
   }
 
@@ -56,6 +88,8 @@ int main(int argc, char** argv) {
     memset(data_portion, 0.0, sizeof data_portion);
     readData(&data_portion[0], portions_info[1], f_in );
   }
+
+
 
   //float data_portion[N];
   //readData(&data_portion[0], N, f_in );
