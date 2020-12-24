c80a33bee57f032168705e53e0a77de4550fa514
polipprilip <tasja.petrova@rambler.ru>
2020-12-24 23:53:22 +0300

    fix int->shor int and add timer^C

--- main.c
+++ main.c
@@ -10,9 +10,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-
+#include <sys/time.h>
 #include "def_types.h"
-
+#include "readWavFuncs.h"
+#include "fftw3.h"
 
 /*
 Путь к wav-файлу можно задать из командной строки,
@@ -21,7 +22,7 @@
 
 int main(int argc, char** argv) {
   
-  char* fin_name = "data/ton_signal.wav";
+  char* fin_name = "ton_signal_4.wav";
   if(argc > 1){
     fin_name = argv[1];
   }
@@ -33,12 +34,62 @@ int main(int argc, char** argv) {
   } else {
     printf("OPEN FILE [%s] is OK \n", fin_name);
   }
-  
-  
-    //Здесь будет Ваш код
 
+  char* f_out_name = "python/output.dat";
+  FILE* f_out = fopen(f_out_name, "wb");
+  if(f_out == NULL){
+    printf("ERROR in OPEN FILE [%s] \n", f_out_name);
+	  return 1;
+  } else {
+    printf("OPEN FILE [%s] is OK \n", f_out_name);
+  }
+
+  // Reading a header of file  
+  t_wavhdr header;
+  readHeader(&header, f_in);
+struct timeval tv_start, tv_end;
+  gettimeofday(&tv_start, NULL);
   
+  
+  int N = 1024;
+
+  // Calculate the number of portions and the tail size
+  int portions_info[2]; //[PortionQuantity, TailSize]
+  calcPortions(&portions_info[0], &header, N);
+
+  // initialising accumulation 
+  int halfN = N;
+  double acc[halfN]; 
+  memset(acc, 0.0, sizeof acc);
+  
+  // Reading portions + FFT + accumulation till the end in cycle
+  int i; double data_portion[N]; double sq[halfN]; 
+  for(i = 0; i < portions_info[0]; ++i){
+      readData(&data_portion[0], N, f_in );
+
+      fftsq(&data_portion[0], &sq[0], halfN);
+ 
+      accumulation(&acc[0], &sq[0], halfN);
+  }
+
+  // Reading tail + FFT + accumulation
+  if (portions_info[1] > 0) {
+    memset(data_portion, 0.0, sizeof data_portion);
+    readData(&data_portion[0], portions_info[1], f_in );
+
+    fftsq(&data_portion[0], &sq[0], halfN);
+ 
+    accumulation(&acc[0], &sq[0], halfN);
+  }
+
+  int a;
+  for (a = 0; a < halfN; ++a)
+    fprintf(f_out, "%f\n", acc[a]);
+
   fclose(f_in);
-  
+  fclose(f_out);
+  gettimeofday(&tv_end, NULL);
+  printf("Time elapsed for programm: %ld microsecs \n", (long int)((tv_end.tv_sec-tv_start.tv_sec)*1000000 + (tv_end.tv_usec - tv_start.tv_usec)));
+    
   return 0;
 }
--- readWavFuncs.c
+++ readWavFuncs.c
@@ -14,47 +14,88 @@
 #include "def_types.h"
 #include "readWavFuncs.h"
 
+//читает структуру заголовка файла f_in в структуру header
+//и выводит информацию из заголовка на экран.
 void readHeader(t_wavhdr* header, FILE* f_in){
-  
-  char* fname = "readHeader";
 
+  fread(header, sizeof(t_wavhdr), 1, f_in);
   
-  //Здесь будет Ваш код
+  //пример печати параметров wav-файла
 
-  
-//пример печати параметров wav-файла
-/*
-	printf("***** Parametrs of WAV-file [%s] *****\n", fin_name);
+  //printf("***** Parametrs of WAV-file [%s] *****\n", fin_name);
 	
-	printf("chunkID =  %s\n", header.chunkID);
-	printf("chunkSize =  %i\n", header.chunkSize);
-	printf("format =  %s\n", header.format);
-	printf("subchunk1ID =  %s\n", header.subchunk1ID);
-	printf("subchunk1Size =  %i\n", header.subchunk1Size);
-	printf("audioFormat =  %i\n", header.audioFormat);
-	printf("numChannels = %i\n", header.numChannels);
-	printf("sampleRate =  %i\n", header.sampleRate);
-	printf("blockAlign = %i\n", header.blockAlign);
-	printf("bitsPerSample = %i\n", header.bitsPerSample);
+  printf("chunkID =  %s\n", header->chunkID);
+  printf("chunkSize =  %i\n", header->chunkSize);
+  printf("format =  %s\n", header->format);
+  printf("subchunk1ID =  %s\n", header->subchunk1ID);
+  printf("subchunk1Size =  %i\n", header->subchunk1Size);
+  printf("audioFormat =  %i\n", header->audioFormat);
+  printf("numChannels = %i\n", header->numChannels);
+  printf("sampleRate =  %i\n", header->sampleRate);
+  printf("blockAlign = %i\n", header->blockAlign);
+  printf("bitsPerSample = %i\n", header->bitsPerSample);
 	
-	printf("subchunk2ID =  %s\n", header.subchunk2ID);
-	printf("subchunk2Size =  %i\n", header.subchunk2Size);
+  printf("subchunk2ID =  %s\n", header->subchunk2ID);
+  printf("subchunk2Size =  %i\n", header->subchunk2Size);
 	
-	printf("*********************************************************\n");
-*/
-  
+  printf("*********************************************************\n");
+
   return;
 }
 
 
+//читает из файла f_in порцию данных размером N в массив data_portion.
+void readData(double* data_portion, int N, FILE* f_in ){
 
-void readData(float* data_portion, int N, FILE* f_in ){
-  
-  char* fname = "readData";
+  short int i, tmp;
+  for (i = 0; i < N; i++){
+    fread(&tmp, sizeof(short int), 1, f_in);
+    data_portion[i]=(double)tmp;
+  } 
 
-  
-  //Здесь будет Ваш код
+  return;
+}
 
+// Return the number of portions and the tail size
+void calcPortions(int* portions_info, t_wavhdr* header, int N){
+
+  int bytesPerSample = bitsToBytes(header->bitsPerSample);
+  int sampleNumber = header->subchunk2Size/(bytesPerSample); // моё предположение - subchunk2Size в два раза больше фактического
+  portions_info[0] = sampleNumber/N; //number of portions without tail
+  portions_info[1] = sampleNumber%N; //size of tail
   
   return;
+}
+
+int bitsToBytes(int bits){
+
+  return bits/8;
+}
+
+void fftsq(double* data_portion, double* sq, int sqsize){
+  fftw_complex out[sqsize];
+  fftw_plan plan;
+  // N = (sqsize-1)*2
+  plan = fftw_plan_dft_r2c_1d((sqsize-1)*2, data_portion, out,  FFTW_ESTIMATE);// FFTW_ESTIMATE or FFTW_MEASURE
+  
+  fftw_execute(plan);
+
+  int j;
+  for(j = 0; j < sqsize; j++){
+    sq[j] = sqrt(out[j][0]*out[j][0]+out[j][1]*out[j][1]);
+  }
+
+  fftw_destroy_plan(plan);
+  
+}
+
+void accumulation(double* acc, double* sq, int sqsize){
+  double c = 4;
+  double koeff  = 1 / c;
+  //acc = (1 - koeff)*acc + koeff*sq;
+
+  int i; 
+  for (i = 0; i < sqsize; ++i){
+    acc[i] = acc[i]*(1 - koeff) + sq[i]*koeff;
+  }
 }
